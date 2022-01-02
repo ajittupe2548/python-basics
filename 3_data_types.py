e959d07e5263e9f6c1af47791b481e6da350d463
ajittupe2548 <ajittupe2548@gmail.com>
2022-01-02 19:15:37 +0530

    Added : DataTypes

---
+++ 3_data_types.py
@@ -0,0 +1,24 @@
+# Data types are the classification or categorization of data items. It represents the kind of value that tells what operations can be performed on a particular data.
+
+age = 22
+year_of_birth = 1999
+is_new_user = True
+favorite_food = ['Kiwi', 'Pizza']
+
+rating = 4.7
+print(rating, type(rating))
+
+name = "Ajit"
+print(name, type(name))
+
+marks = 98
+print(marks, type(marks))
+
+is_passed = True
+print(is_passed, type(is_passed))
+
+my_complex_number = 4 + 5j
+print(my_complex_number, type(my_complex_number))
+
+fruits_list = ['Apple', 'Banana', 'Kiwi']
+print(fruits_list, type(fruits_list))
