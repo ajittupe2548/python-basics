b89ce53424ade3ce92ddb915b5b015ad18d58e93
ajittupe2548 <ajittupe2548@gmail.com>
2022-01-02 19:30:42 +0530

    Added : Operators

---
+++ 4_operators.py
@@ -0,0 +1,47 @@
+# Operators are used to perform operations on variables and values.
+
+# Arithmatic operators
+
+# a = 45
+# b = 45
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a//b)
+# print(a**b)
+
+
+# Assignment operators
+
+# a =  6
+# a = a + 5
+# a += 5
+# a -= 5
+# a *= 5
+# a /= 5
+
+# print(a)
+
+# Relational operators
+# a = 45
+# b = 23
+
+# print('a > b', a > b)
+# print('a < b', a < b)
+# print('a >= b', a >= b)
+# print('a <= b', a <= b)
+# print('a == b', a == b)
+# print('a != b', a != b)
+
+# Logical operators
+
+x = True
+y = True
+
+print('x and y is',x and y)
+
+print('x or y is',x or y)
+
+print('not x is',not x)
