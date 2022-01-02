4db85058a1ec3d7e1cfe0dd597db82950877379d
ajittupe2548 <ajittupe2548@gmail.com>
2022-01-02 19:32:41 +0530

    Added : User inputs

---
+++ 5_user_input.py
@@ -0,0 +1,7 @@
+english_marks = int(input('Enter marks in English\n'))
+maths_marks = int(input('Enter marks in Maths\n'))
+science_marks = int(input('Enter marks in Science\n'))
+
+average_marks = (english_marks + science_marks + maths_marks) / 3
+
+print('average marks are', average_marks)
